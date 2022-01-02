22a30f4093c14b5eed382412fb92eb19d90cfcd3
HimuraKrd <68470186+HimuraKrd@users.noreply.github.com>
2022-01-02 12:01:07 +0300

    Create base_vm.tf

---
+++ 07-terraform-03-basic/base_vm.tf
@@ -0,0 +1,30 @@
+resource "yandex_compute_instance" "node01" {
+  name                      = "node01"
+  zone                      = "ru-central1-a"
+  hostname                  = "node01.netology.cloud"
+  allow_stopping_for_update = true
+
+  resources {
+    cores  = 2
+    memory = 2
+    core_fraction = 20
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id    = "fd850g3ak2utqntpgarf"
+      name        = "root-node01"
+      type        = "network-nvme"
+      size        = "10"
+    }
+  }
+
+  network_interface {
+    subnet_id = "${yandex_vpc_subnet.default.id}"
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "centos:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
